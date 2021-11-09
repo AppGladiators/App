e7037ef7ba1480a4ef8d51bfc577dec24f50317a
Tunishavyas <77680297+Tunishavyas@users.noreply.github.com>
2021-11-09 23:09:07 +0530

    Update contact_us.dart

--- Contact us/contact_us.dart
+++ Contact us/contact_us.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -12,129 +11,129 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-          appBar: AppBar(
-            title: Text('Contact Us') ,
-          ),
-           
-
-         drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF0091DE),
-              ),
-              child: Column(
+            appBar: AppBar(
+              title: Text('Contact Us'),
+            ),
+            drawer: Drawer(
+              child: ListView(
                 children: <Widget>[
-                  CircleAvatar(
-                    radius: 40.0,
-                    backgroundColor: const Color(0xff0091DE),
-                    child: Image.network('https://images.static-collegedunia.com/public/college_data/images/logos/1559633020logo.jpg'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      "MATA SUNDRI COLLEGE FOR WOMEN",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12.0,
-                      ),
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF0091DE),
+                    ),
+                    child: Column(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: 40.0,
+                          backgroundColor: const Color(0xff0091DE),
+                          child: Image.network('https://images.static-collegedunia.com/public/college_data/images/logos/1559633020logo.jpg'),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            "MATA SUNDRI COLLEGE FOR WOMEN",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 12.0,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 8.0),
+                          child: Text(
+                            "University of Delhi",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 10.0,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Padding(
-                    padding: EdgeInsets.only(top: 8.0),
-                    child: Text(
-                      "University of Delhi",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 10.0,
-                      ),
-                    ),
-                  ),
+                  CustomListStyle(Icons.home, 'HOMEPAGE', () => {}),
+                  CustomListStyle(Icons.info, 'ABOUT MSCW', () => {}),
+                  CustomListStyle(Icons.people, 'SOCIETIES', () => {}),
+                  CustomListStyle(Icons.highlight, 'EVENT HIGHLIGHT', () => {}),
+                  CustomListStyle(Icons.headset_mic, 'RAISE AN ISSUE', () => {}),
+                  CustomListStyle(Icons.web, 'VISIT OUR WEBSITE', () => {}),
+                  CustomListStyle(Icons.call, 'CONTACT US', () => {}),
+                  CustomListStyle(Icons.home, 'PUBLISH OPPORTUNITY', () => {}),
+                  CustomListStyle(Icons.login, 'ADMIN LOGIN', () => {}),
+                  CustomListStyle(Icons.help, 'FAQ\'s', () => {}),
                 ],
               ),
             ),
-            CustomListStyle(Icons.home, 'HOMEPAGE', () => {}),
-            CustomListStyle(Icons.info, 'ABOUT MSCW', () => {}),
-            CustomListStyle(Icons.people, 'SOCIETIES', () => {}),
-            CustomListStyle(Icons.highlight, 'EVENT HIGHLIGHT', () => {}),
-            CustomListStyle(Icons.headset_mic, 'RAISE AN ISSUE', () => {}),
-            CustomListStyle(Icons.web, 'VISIT OUR WEBSITE', () => {}),
-            CustomListStyle(Icons.call, 'CONTACT US', () => {}),
-            CustomListStyle(Icons.home, 'PUBLISH OPPORTUNITY', () => {}),
-            CustomListStyle(Icons.login, 'ADMIN LOGIN', () => {}),
-            CustomListStyle(Icons.help, 'FAQ\'s', () => {}),
-          ],
-        ),
-      ),
-        bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF0091DE),
-        selectedItemColor: Color(0xFFFFBA15),
-        unselectedItemColor: Color(0xFFF1F2F2),
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.group),
-            label: 'Soceities',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.event),
-            label: 'Events',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About Us',
-          ),
-        ],
-        
-        ),
-         
+            bottomNavigationBar: BottomNavigationBar(
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.home,
+                    color: Color(0xFF6C6C6C),
+                  ),
+                  title: Text('Home'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.people, color: Color(0xFF6C6C6C)),
+                  title: Text('Socities'),
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.highlight, color: Color(0xFF6C6C6C)),
+                  title: Text('Events'),
+                ),
+              ],
+            ),
+
             //backgroundColor: Colors.yellow[250],
             body: SafeArea(
-                child: Column(   
+                child: Column(
               children: [
-                 SizedBox(
-            height: 12.0,),
-                ClipRRect(borderRadius: BorderRadius.circular(20),
-                child:Image.asset('images/img.jpg',
-                // width:200,
-                // height:200,
-                // fit:BoxFit.cover,
+                SizedBox(
+                  height: 12.0,
                 ),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                      'https://th.bing.com/th/id/R.ad87952193bbbba244aa319c990c266e?rik=qOCC54B9qQAz3Q&riu=http%3a%2f%2fmscw.ac.in%2fresources%2fimg%2fslider%2fcol2.jpg&ehk=7CE1DKAp2EXaQj0C3Tn4kSYMZ0R%2fihTU3NbD%2bNbQxp8%3d&risl=&pid=ImgRaw&r=0',
+                      width: 400,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
                 ),
-                Card(     
+                Card(
                   //padding:EdgeInsets.all(10.0),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
-                  color: Colors.grey[200],
+                  margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
+                  color: Color(0xFFF0F0F0),
                   child: Row(
                     children: [
-                      
                       Center(
                         child: Center(
                           child: Container(
-                            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(7.0)),color: Colors.yellow,),
-                            
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                              color: Color(0xFFFFBA15),
+                            ),
                             child: Icon(
                               Icons.call,
-                              color: Colors.black,
+                              color: Color(0xFF6C6C6C),
                               size: 30.0,
                             ),
                           ),
                         ),
                       ),
-                      
                       SizedBox(
                         width: 20.0,
                       ),
-                      Text('23236102'),
-                      
+                      Text('23236102', 
+                      style:GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                                 color: Color(0xFF525252),
+                                 fontSize: 13,),
+                      ),
+                      ),
                     ],
                   ),
                 ),
@@ -142,64 +141,74 @@ class MyApp extends StatelessWidget {
                   height: 5.0,
                 ),
                 Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
+                  margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
                   color: Colors.grey[200],
                   child: Row(
                     children: [
-                      
-                      Container(
-                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(7.0)),color: Colors.yellow,),
-                        child: Icon(
-                          Icons.phone_android_outlined,
-                          color: Colors.black,
-                          size: 30.0,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 20.0,
-                      ),
-                      Text('23221449, 23237291'),
-                    ],
-                  ),
-                ),
-                Card(     
-                  //padding:EdgeInsets.all(10.0),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
-                  color: Colors.grey[200],
-                  child: Row(
-                    children: [
-                      
                       Center(
                         child: Center(
                           child: Container(
-                            decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(7.0)),color: Colors.yellow,),
-                            
+                             padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                              color: Color(0xFFFFBA15),
+                            ),
                             child: Icon(
-                              Icons.mail,
-                              color: Colors.black,
+                              Icons.phone_android_outlined,
+                              color: Color(0xFF6C6C6C),
                               size: 30.0,
                             ),
                           ),
                         ),
                       ),
-                      
+                      SizedBox(
+                        width: 20.0,
+                      ),
+                      Text('23221449, 23237291',
+                      style:GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                                 color: Color(0xFF525252),
+                                 fontSize: 13,),
+                      ),
+                      ),
+                    ],
+                  ),
+                ),
+                Card(
+                  //padding:EdgeInsets.all(10.0),
+                  margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
+                  color: Colors.grey[200],
+                  child: Row(
+                    children: [
+                      Container(
+                         padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                          color: Color(0xFFFFBA15),
+                        ),
+                        child: Icon(
+                          Icons.mail,
+                          color: Color(0xFF6C6C6C),
+                          size: 30.0,
+                        ),
+                      ),
                       SizedBox(
                         width: 20.0,
                       ),
                       Column(
                         children: [
-                          Text('Principal Office:', style: GoogleFonts.roboto(
-                    textStyle:
-                            TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
-                  ),
-                  ),
-                  
-                           Text(' matasundricollege.du@gmail.com'),
+                          Text(
+                            'Principal Office:\nmatasundricollege.du@gmail.com',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                color: Color(0xFF525252),
+                                fontSize: 13,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                      
                     ],
                   ),
                 ),
@@ -207,67 +216,77 @@ class MyApp extends StatelessWidget {
                   height: 5.0,
                 ),
                 Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
+                  margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
                   color: Colors.grey[200],
                   child: Row(
-                    
                     children: [
-                      
                       Container(
-                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(7.0)),color: Colors.yellow,),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                          color:Color(0xFFFFBA15),
+                        ),
                         child: Icon(
                           Icons.mail,
-                          color: Colors.black,
+                          color: Color(0xFF6C6C6C),
                           size: 30.0,
                         ),
                       ),
                       SizedBox(
                         width: 20.0,
                       ),
-                      
                       Column(
                         children: [
-                          Text('Admin Office:', style: GoogleFonts.roboto(
-                    textStyle:
-                            TextStyle(fontSize: 15,fontWeight: FontWeight.bold),
-                  ),
-                  ),
-                           Text('mscadmin@ms.du.ac.in'),
+                          Text(
+                            'Admin Office:\nmscadmin@ms.du.ac.in',
+                            textAlign: TextAlign.left,
+                            style: GoogleFonts.roboto(
+                              textStyle: TextStyle(
+                                 color: Color(0xFF525252),
+                                fontSize: 13,
+                              ),
+                            ),
+                          ),
                         ],
                       ),
-                      
                     ],
                   ),
                 ),
-                 Card(
-                  margin:
-                      EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 13.0, horizontal: 13.0),
                   color: Colors.grey[200],
                   child: Row(
                     children: [
-                      
                       Container(
-                        decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(7.0)),color: Colors.yellow,),
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(7.0)),
+                          color: Color(0xFFFFBA15),
+                        ),
                         child: Icon(
                           Icons.language_outlined,
-                          color: Colors.black,
+                          color: Color(0xFF6C6C6C),
                           size: 30.0,
                         ),
                       ),
                       SizedBox(
                         width: 20.0,
                       ),
-                      Text('Visit Our Website'),
+                      Text('Visit Our Website',
+                      style:GoogleFonts.roboto(
+                        textStyle: TextStyle(
+                                 color: Color(0xFF525252),
+                                 fontSize: 13,),
+                      ),
+                      ),
                     ],
                   ),
                 ),
-                
               ],
-            ))
-            ));
+            ))));
   }
 }
+
 class CustomListStyle extends StatelessWidget {
   IconData icon;
   String text;
